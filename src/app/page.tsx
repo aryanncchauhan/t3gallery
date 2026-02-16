3807c1e0396304344ab7c82d18b102839e57ef0c
Aryan <aryanc0809@gmail.com>
2026-02-16 22:28:33 +1100

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://3l9foyyg1h.ufs.sh/f/meOdMfknKMNyxXrxrDtUkb6TO97wsdKiRfcuZvHlMqjxnpPh",
@@ -13,10 +14,16 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log("Posts:", posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} alt="image" className="aspect-video object-cover" />
