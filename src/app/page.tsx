1a4a68c96339ca99dab0730e4de004a30fe7cb76
Aryan <aryanc0809@gmail.com>
2026-02-16 21:41:24 +1100

    display simple row of images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://3l9foyyg1h.ufs.sh/f/meOdMfknKMNyxXrxrDtUkb6TO97wsdKiRfcuZvHlMqjxnpPh",
+  "https://3l9foyyg1h.ufs.sh/f/meOdMfknKMNyhyPI66FOvHm8f2Un9z6bKajDVBtFlCqLxTWi",
+  "https://3l9foyyg1h.ufs.sh/f/meOdMfknKMNywRQ5TAoO2iFNDHfnbzUvCtuyrsRIB9cZ1qJj",
+  "https://3l9foyyg1h.ufs.sh/f/meOdMfknKMNyB6ROoPmiCvDejpQyrunlcPtVk7YR530EmwK1",
+  "https://3l9foyyg1h.ufs.sh/f/meOdMfknKMNyUBtMzp4szwXvo7Eq8htYnaRZQ0dxk9Up3TuF"
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image" className="aspect-video object-cover" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
