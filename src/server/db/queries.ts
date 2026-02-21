3f6df4afc53a8e27b04726e745635c84f409c7ce
Aryan <aryanc0809@gmail.com>
2026-02-21 20:08:22 +1100

    break queries into separate file

---
+++ src/server/db/queries.ts
@@ -0,0 +1,17 @@
+import "server-only";
+import { db } from ".";
+import { auth } from "@clerk/nextjs/server";
+
+
+export async function getMyImages() {
+  const user = await auth();
+
+  if (!user.userId) throw new Error("Unauthorised");
+
+  const images = await db.query.images.findMany({
+    where: (model, { eq }) => eq(model.userId, user.userId),
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+
+  return images;
+}
