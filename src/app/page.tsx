f9a96502d0e4b6a791bed2e8f16c07af3018c2c3
Aryan <aryanc0809@gmail.com>
2026-03-08 14:40:53 +1100

    Normalize line endings to CRLF

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ async function Images() {
 
   return (
     <div className="flex flex-wrap justify-center gap-4 p-4">
-      {[...images, ...images, ...images].map((image, index) => (
-        <div key={`${image.id}-${index}`} className="flex h-48 w-48 flex-col">
+      {images.map((image, index) => (
+        <div key={image.id} className="flex h-48 w-48 flex-col">
           <div className="relative h-48 w-48">
             <Link href={`/img/${image.id}`}>
               <Image
